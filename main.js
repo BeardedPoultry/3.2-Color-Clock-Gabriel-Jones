4b984f6f7354fa9985f471f323a2e326cd511227
badview08 <jonesgabriel21@gmail.com>
2015-05-19 16:03:23 -0400

    date work coming along, not fully done

--- main.js
+++ main.js
@@ -1,22 +1,15 @@
-
-
-var element = document.getElementById('example-clock-container');
-
-var seconds = new ProgressBar.Circle(element, {
-    duration: 200,
-    color: "#FCB03C",
-    trailColor: "#ddd"
-});
-
-setInterval(function() {
-    var second = new Date().getSeconds();
-    seconds.animate(second / 60, function() {
-        seconds.setText(second);
-    });
-}, 1000);
-var line = new ProgressBar.Line('#example-line-container', {
-    color: '#FCB03C'
-
-});
-
-line.animate(1.0); 
+var timeNow;
+var display = [];  
+function getTotalTime() {
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+  
+  console.log(hours, minutes, seconds);
+  
+//  $hours = Date.prototype.getHours('.hours');
+//  $minutes = Date.prototype.getMinutes('.minutes');
+//  seconds = Date.prototype.getSeconds('.seconds');
+}
+getTotalTime();
