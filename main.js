860aad4d5e3f5ba7f49679386d98bfab28df6b2e
badview08 <jonesgabriel21@gmail.com>
2015-05-19 18:12:23 -0400

    experimenting with converting clock to rgb color value

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 var timeNow;
-var display = [];  
+var $timeCurrent = document.querySelector(".timeCurrent");
+
 function getTotalTime() {
+  
   var currentTime = new Date();
   var hours = currentTime.getHours();
   var minutes = currentTime.getMinutes();
   var seconds = currentTime.getSeconds();
   
-  console.log(hours, minutes, seconds);
+  hours = (hours < 10 ? "0" : '') + hours;
+  minutes = (minutes < 10 ? "0" : '') + minutes;
+  seconds = (seconds < 10 ? "0" : '') + seconds;
+
+  $timeCurrent.textContent = hours + " : " + minutes + " : " + seconds;
   
-//  $hours = Date.prototype.getHours('.hours');
-//  $minutes = Date.prototype.getMinutes('.minutes');
-//  seconds = Date.prototype.getSeconds('.seconds');
+  console.log(hours, minutes, seconds);
 }
+
 getTotalTime();
+setInterval(getTotalTime, 1000);
+var ProgressBar = $seconds;
+var line = new ProgressBar.Line('#example-line-container', {
+    color: '#FCB03C'
+
+});
+
+line.animate(1.0);
